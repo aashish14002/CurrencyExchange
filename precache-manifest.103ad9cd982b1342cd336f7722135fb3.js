fee0e2962dd32a0d10edf444659183fd2b7955db
Aashish Kumar <aashish14002@iiitd.ac.in>
2021-01-19 23:59:18 +0100

    deploy

---
+++ precache-manifest.103ad9cd982b1342cd336f7722135fb3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552dd8dd03bc1e4ff3f7",
+    "url": "/CurrencyExchange/css/app.9b5db73d.css"
+  },
+  {
+    "revision": "824d2ec989f34865ea0dba9359393f59",
+    "url": "/CurrencyExchange/img/convert.824d2ec9.png"
+  },
+  {
+    "revision": "20011c15e7ab10e88cd67f7d6b52d3a3",
+    "url": "/CurrencyExchange/img/logo.20011c15.png"
+  },
+  {
+    "revision": "64723749a60d4f028dc0f106d49550f1",
+    "url": "/CurrencyExchange/index.html"
+  },
+  {
+    "revision": "552dd8dd03bc1e4ff3f7",
+    "url": "/CurrencyExchange/js/app.5e4301c5.js"
+  },
+  {
+    "revision": "ea71c41f16ed5d4a99e7",
+    "url": "/CurrencyExchange/js/chunk-vendors.81f1398a.js"
+  },
+  {
+    "revision": "20011c15e7ab10e88cd67f7d6b52d3a3",
+    "url": "/CurrencyExchange/logo.png"
+  },
+  {
+    "revision": "b387e490947dc692aa356a7c00b84452",
+    "url": "/CurrencyExchange/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/CurrencyExchange/robots.txt"
+  }
+]);
---
+++ serviceWorker.js
@@ -0,0 +1,28 @@
+importScripts("/CurrencyExchange/precache-manifest.103ad9cd982b1342cd336f7722135fb3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+import {registerRoute} from 'workbox-routing';
+import {CacheFirst} from 'workbox-strategies';
+import {CacheableResponsePlugin} from 'workbox-cacheable-response';
+import {ExpirationPlugin} from 'workbox-expiration';
+
+registerRoute(
+  ( { url } ) => url.origin === 'https://api.exchangeratesapi.io/latest/' ||
+              url.origin === 'https://api.binance.com/api/v3/',
+              
+  //If there is a Response in the cache, the Request will be fulfilled 
+  //using the cached response and the network will not be used at all. 
+  //If there isn't a cached response, the Request will be fulfilled by
+  //a network request and the response will be cached so that the next
+  //request is served directly from the cache.
+  new CacheFirst( {
+    cacheName: 'currency-exchange-cache',
+    plugins: [
+      new CacheableResponsePlugin( {
+        statuses: [ 0, 200 ],
+      } ),
+      new ExpirationPlugin( {
+        maxAgeSeconds: 24 * 60 * 60,
+      } )
+    ]
+  } )
+);
